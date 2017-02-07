8975451b38948c1cd74a57e3a7f230159dcbb1f7
fee1in <632087409@qq.com>
2017-02-07 20:43:29 +0800

    add 5 fragment

---
+++ app/src/main/java/com/itheima/redboyclient/fragment/BaseFragment.java
@@ -0,0 +1,26 @@
+package com.itheima.redboyclient.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BaseFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(this.getClass().getSimpleName());
+        return textView;
+    }
+
+}
