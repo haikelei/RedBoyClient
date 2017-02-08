89194b9aa45583cf38f5901c67efbfa1d3841345
itsjk <979284252@qq.com>
2017-02-08 15:03:25 +0800

    Merge branch 'lujialei' of https://github.com/haikelei/RedBoyClient into sjk
    # Conflicts:
    #	.idea/gradle.xml
    #	.idea/libraries/support_v4_23_0_1.xml
    #	app/app.iml
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/itheima/redboyclient/utils/ConstantsRedBaby.java
    #	app/src/main/res/layout/register_activity.xml
    #	app/src/main/res/values/strings.xml
    #	devpkg-android/build.gradle
    #	devpkg-android/devpkg-android.iml

---
+++ app/src/main/java/com/itheima/redboyclient/present/RegisterPresenter.java
@@ -0,0 +1,9 @@
+package com.itheima.redboyclient.present;
+
+/**
+ * Created by sjk on 2016/12/26.
+ */
+public interface RegisterPresenter {
+    //注册的逻辑应该在p层
+    void regist(String username, String pwd);
+}
