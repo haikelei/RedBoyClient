a6695d1a73c6964ba30e682799cda10138a561d3
mayuxuan <306524254@qq.com>
2017-02-08 17:12:58 +0800

    添加推荐品牌

--- app/src/main/java/com/itheima/redboyclient/utils/ConstantsRedBaby.java
+++ app/src/main/java/com/itheima/redboyclient/utils/ConstantsRedBaby.java
@@ -3,11 +3,13 @@ package com.itheima.redboyclient.utils;
 public class ConstantsRedBaby {
     public static final String URL_SERVER = "http://192.168.56.1:8080/RedBabyServer";
     public static final String URL_TOPIC = URL_SERVER + "/topic";
+    public static final String URL_BRAND = URL_SERVER + "/brand";
 
 
     /**
      * topic请求码
      */
     public static final int REQUEST_CODE_TOPIC = 0;
+    public static final int REQUEST_CODE_BRAND = 9;
 
 }
