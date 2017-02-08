8531c913ea7ab528978c8048469b1f2b1f82df74
lujialei <562759989@qq.com>
2017-02-08 12:14:53 +0800

    Merge branch 'master' of https://github.com/haikelei/RedBoyClient into lujialei
    # Conflicts:
    #	.gradle/2.10/taskArtifacts/cache.properties.lock
    #	.gradle/2.10/taskArtifacts/fileHashes.bin
    #	.gradle/2.10/taskArtifacts/fileSnapshots.bin
    #	.gradle/2.10/taskArtifacts/outputFileStates.bin
    #	.gradle/2.10/taskArtifacts/taskArtifacts.bin
    #	.idea/libraries/support_v4_23_0_1.xml
    #	app/app.iml
    #	local.properties

--- app/src/main/java/com/itheima/redboyclient/present/RegisterPresenter.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.redboyclient.present;
-
-/**
- * Created by sjk on 2016/12/26.
- */
-public interface RegisterPresenter {
-    //注册的逻辑应该在p层
-    void regist(String username, String pwd);
-}
