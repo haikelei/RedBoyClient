f44b40e1061a5a52a4d4c45192482507f8dea091
ma422213631 <422213631@qq.com>
2017-02-08 20:32:55 +0800

    ADD NEWPRODUCT2

--- app/src/main/java/com/itheima/redboyclient/utils/ConstantsRedBaby.java
+++ app/src/main/java/com/itheima/redboyclient/utils/ConstantsRedBaby.java
@@ -9,6 +9,7 @@ public class ConstantsRedBaby {
     public static final String URL_LOGIN = URL_SERVER +"/login";
     public static final String URL_SEARCH_RECOMMEND = URL_SEARCH +"/recommend";
     public static final String URL_BRAND = URL_SERVER + "/brand";
+    public static final String URL_NEWPRODUCT = URL_SERVER + "/newproduct";
 
 
 
@@ -23,6 +24,7 @@ public class ConstantsRedBaby {
     public static final int REQUEST_CODE_BRAND = 4;
     public static final int REQUEST_CODE_SEARCH = 5;
     public static final int REQUEST_CODE_RECOMMEND = 6;
+    public static final int REQUEST_NEW_PRODUCT = 7;
 
 
 }
