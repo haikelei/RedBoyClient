fe6c00045d3d4b607bf412d3e452cf6d1b68e5cc
lujialei <562759989@qq.com>
2017-02-08 15:31:58 +0800

    Merge branch 'master' of https://github.com/haikelei/RedBoyClient into lujialei
    # Conflicts:
    #	.gradle/2.10/taskArtifacts/cache.properties.lock
    #	.gradle/2.10/taskArtifacts/fileHashes.bin
    #	.gradle/2.10/taskArtifacts/fileSnapshots.bin
    #	.gradle/2.10/taskArtifacts/outputFileStates.bin
    #	.gradle/2.10/taskArtifacts/taskArtifacts.bin
    #	.idea/libraries/support_v4_23_0_1.xml
    #	app/app.iml
    #	local.properties

--- app/src/main/java/com/itheima/redboyclient/fragment/ShoppingFragment.java
+++ app/src/main/java/com/itheima/redboyclient/fragment/ShoppingFragment.java
@@ -1,13 +1,19 @@
 package com.itheima.redboyclient.fragment;
 
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
-
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ShoppingFragment extends BaseFragment {
-
-
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //View view = inflater.inflate(R.layout.)
+        return null;
+    }
 }
