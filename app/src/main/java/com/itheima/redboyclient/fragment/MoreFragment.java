edbc747cbcd09b55f5c5bad2898f91af8015ff47
fee1in <632087409@qq.com>
2017-02-09 15:16:51 +0800

    Merge branch 'master' of https://github.com/haikelei/RedBoyClient into linhui
    # Conflicts:
    #	.gradle/2.10/taskArtifacts/cache.properties.lock
    #	.gradle/2.10/taskArtifacts/fileHashes.bin
    #	.gradle/2.10/taskArtifacts/fileSnapshots.bin
    #	.gradle/2.10/taskArtifacts/outputFileStates.bin
    #	.gradle/2.10/taskArtifacts/taskArtifacts.bin
    #	app/app.iml
    #	app/src/main/java/com/itheima/redboyclient/fragment/BaseFragment.java
    #	app/src/main/java/com/itheima/redboyclient/fragment/MoreFragment.java

--- app/src/main/java/com/itheima/redboyclient/fragment/MoreFragment.java
+++ app/src/main/java/com/itheima/redboyclient/fragment/MoreFragment.java
@@ -3,14 +3,14 @@ package com.itheima.redboyclient.fragment;
 
 import android.support.v4.app.Fragment;
 
+import com.itheima.redboyclient.R;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MoreFragment extends MainBaseFragment {
 
-
-public class MoreFragment extends BaseFragment {
     @Override
     protected int getRootViewId() {
         return R.layout.activity_temp;
