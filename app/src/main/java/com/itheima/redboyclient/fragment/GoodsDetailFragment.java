0c02f39b27838bf2b9bea0ce9be119df371eb90a
lujialei <562759989@qq.com>
2017-02-12 11:29:31 +0800

    Merge branch 'master' of https://github.com/haikelei/RedBoyClient into lujialei正常

--- app/src/main/java/com/itheima/redboyclient/fragment/GoodsDetailFragment.java
+++ app/src/main/java/com/itheima/redboyclient/fragment/GoodsDetailFragment.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.AppCompatSpinner;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -20,8 +21,11 @@ import com.itheima.redboyclient.R;
 import com.itheima.redboyclient.adapter.GoodDetailVPAdapter;
 import com.itheima.redboyclient.net.resp.GoodResponse;
 
+import java.util.ArrayList;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 
 
 /**
@@ -100,16 +104,51 @@ public class GoodsDetailFragment extends Fragment {
         vp.setAdapter(adapter);
         //标题等设置
         tvTitle.setText(goodResponse.getProduct().getName());
-        tvMarketPrice.setText("市场价：¥ "+goodResponse.getProduct().getMarketPrice()+"");
-        tvPrice.setText("会员价：¥ "+goodResponse.getProduct().getPrice()+"");
+        tvMarketPrice.setText("市场价：¥ " + goodResponse.getProduct().getMarketPrice() + "");
+        tvPrice.setText("会员价：¥ " + goodResponse.getProduct().getPrice() + "");
         ratingBar.setRating(goodResponse.getProduct().getScore());
-        tvArea.setText("查看库存："+goodResponse.getProduct().getInventoryArea()+"(有货)");
-        tvCommentCount.setText("用户评论：共有"+goodResponse.getProduct().getCommentCount()+"人评论");
+        tvArea.setText("查看库存：" + goodResponse.getProduct().getInventoryArea() + "(有货)");
+        tvCommentCount.setText("用户评论：共有" + goodResponse.getProduct().getCommentCount() + "人评论");
+        //下拉框设置
+        ArrayList<String> corlors = new ArrayList<>();
+        ArrayList<String> sizes = new ArrayList<>();
+        ArrayList<GoodResponse.ProductBean.ProductPropertyBean> beans =
+                (ArrayList<GoodResponse.ProductBean.ProductPropertyBean>) goodResponse.getProduct().getProductProperty();
+        for (GoodResponse.ProductBean.ProductPropertyBean bean : beans) {
+            if("颜色".equals(bean.getK())){
+                corlors.add(bean.getV());
+            } else if("尺码".equals(bean.getK())){
+                sizes.add(bean.getV());
+            }
+        }
+
+        ArrayAdapter corlorArrayAdapter = new ArrayAdapter(getContext(), R.layout.spinner_item, corlors);
+        spinnerCorlor.setAdapter(corlorArrayAdapter);
+        ArrayAdapter sizeArrayAdapter = new ArrayAdapter(getContext(), R.layout.spinner_item, sizes);
+        spinnerSize.setAdapter(sizeArrayAdapter);
+
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         ButterKnife.reset(this);
+    }
+
+    @OnClick({R.id.bt_minus, R.id.bt_plus})
+    public void onClick(View view) {
+        String s = etNum.getText().toString();
+        int count = Integer.parseInt(s);
+        switch (view.getId()) {
+            case R.id.bt_minus:
+                if(count <= 1){
+                    return;
+                }
+                etNum.setText(--count+"");
+                break;
+            case R.id.bt_plus:
+                etNum.setText(++count+"");
+                break;
+        }
     }
 }
